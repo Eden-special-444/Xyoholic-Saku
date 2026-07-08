2c130198abe1cbaae73428c352851529358aee27
Eden-special-444 <singaporeserver9092@gmail.com>
2026-07-08 20:32:04 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ ADMIN: 𝐒𝐚𝐤𝐮 𝐁𝐞𝐢𝐧𝐠'𝐝𝐞𝐚𝐭𝐡𝐨𝐥𝐢𝐜 𝐈𝐈 彡\n├‣ WHATSAPP\n╰‣ 01774794242`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
