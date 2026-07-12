310055f7faca4f94769c2c7159140a5ae1009a0a
Eden-special-444 <singaporeserver9092@gmail.com>
2026-07-12 11:46:07 +0600

    Update marry.js

--- scripts/cmds/marry.js
+++ scripts/cmds/marry.js
@@ -7,7 +7,7 @@ module.exports = {
 		name: "marry",
 		aliases: ["marryv4", "marryfour"],
 		version: "1.0",
-		author: "\x4c\x45\x41\x52\x4e\x20\x54\x4f\x20\x45\x41\x54\x20\x4c\x45\x41\x52\x4e\x20\x54\x4f\x20\x53\x50\x45\x41\x4b\x20\x42\x55\x54\x20\x44\x4f\x4e\x27\x54\x20\x54\x52\x59\x20\x54\x4f\x20\x43\x48\x41\x4e\x47\x45\x20\x54\x48\x45\x20\x43\x52\x45\x44\x49\x54\x20\x41\x4b\x41\x53\x48",
+		author: "RAHAT ",
 		countDown: 5,
 		role: 0,
 		shortDescription: "get a wife",
@@ -21,7 +21,9 @@ module.exports = {
 			console.log("Body:", event.body);
 			console.log("Mentions:", event.mentions);
 
-			const mention = Object.keys(event.mentions || {});
+			// Fix: Check if mentions exists and is an object
+			const mentions = event.mentions || {};
+			const mention = Object.keys(mentions);
 
 			if (!mention.length) {
 				return message.reply("Please mention someone❗");
@@ -29,6 +31,11 @@ module.exports = {
 
 			const one = event.senderID;
 			const two = mention[0];
+
+			// Check if the mentioned ID is valid
+			if (!two || two === one) {
+				return message.reply("Please mention someone else ❗");
+			}
 
 			const ptth = await bal(one, two);
 
@@ -44,28 +51,33 @@ module.exports = {
 };
 
 async function bal(one, two) {
-	let avone = await jimp.read(
-		`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
-	);
-	avone.circle();
+	try {
+		let avone = await jimp.read(
+			`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+		);
+		avone.circle();
 
-	let avtwo = await jimp.read(
-		`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
-	);
-	avtwo.circle();
+		let avtwo = await jimp.read(
+			`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+		);
+		avtwo.circle();
 
-	const pth = "marryv4.png";
+		const pth = "marryv4.png";
 
-	let img = await jimp.read(
-		"https://i.postimg.cc/XN1TcH3L/tumblr-mm9nfpt7w-H1s490t5o1-1280.jpg"
-	);
+		let img = await jimp.read(
+			"https://i.postimg.cc/XN1TcH3L/tumblr-mm9nfpt7w-H1s490t5o1-1280.jpg"
+		);
 
-	img
-		.resize(1024, 684)
-		.composite(avone.resize(85, 85), 204, 160)
-		.composite(avtwo.resize(80, 80), 315, 105);
+		img
+			.resize(1024, 684)
+			.composite(avone.resize(85, 85), 204, 160)
+			.composite(avtwo.resize(80, 80), 315, 105);
 
-	await img.writeAsync(pth);
+		await img.writeAsync(pth);
 
-	return pth;
+		return pth;
+	} catch (error) {
+		console.error("Error in bal function:", error);
+		throw error;
+	}
 }
